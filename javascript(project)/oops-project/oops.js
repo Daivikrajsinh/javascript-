d99e9d28e18addf9797babf242f1bb087d5d8e34
Daivikrajsinh <devikrajsinh05@gmail.com>
2024-12-27 13:05:44 +0530

    java-script

--- javascript(project)/oops-project/oops.js
+++ javascript(project)/oops-project/oops.js
@@ -85,9 +85,7 @@ class Vehicle {
   car.Details();
 
 
-
-
-  // question-4
+// question-4
 class BankAccount {
   constructor(accountNumber, balance = 0) {
     this.accountNumber = accountNumber;
@@ -95,35 +93,27 @@ class BankAccount {
   }
 
   deposit(amount) {
-    if (amount > 0) {
-      this.balance += amount;
-      console.log(`Deposited $${amount} into account ${this.accountNumber}. New balance: $${this.balance}`);
-    } else {
-      console.log("Deposit amount must be positive.");
-    }
+    this.balance += amount;
   }
 
   withdraw(amount) {
-    if (amount > 0 && amount <= this.balance) {
+    if (this.balance >= amount) {
       this.balance -= amount;
-      console.log(`Withdrew $${amount} from account ${this.accountNumber}. New balance: $${this.balance}`);
-    } else if (amount > this.balance) {
-      console.log("Insufficient funds for withdrawal.");
     } else {
-      console.log("Withdrawal amount must be positive.");
+      console.log("Insufficient funds");
     }
   }
 }
+let account1 = new BankAccount("11334", 2000);
+let account2 = new BankAccount("66778", 5000);
 
-const account1 = new BankAccount(101, 500);
-const account2 = new BankAccount(102);
+account1.deposit(500);
+account2.deposit(200);
+account1.withdraw(300);
+account2.withdraw(700);
 
-account1.deposit(200); 
-account1.withdraw(150); 
-account1.withdraw(700); 
-
-account2.deposit(300); 
-account2.withdraw(50);  
+console.log(account1);
+console.log(account2);
 
 
 
@@ -162,3 +152,106 @@ console.log(`The area of the circle is: ${circle.calculateArea().toFixed(2)}`);
 
 const triangle = new Triangle(10, 6);
 console.log(`The area of the triangle is: ${triangle.calculateArea()}`);
+
+
+
+
+// question-6
+
+
+class Employee {
+  constructor(name, salary) {
+      this.name = name;
+      this.salary = salary; 
+  }
+
+  calculateAnnualSalary() {
+      return this.salary * 12;
+  }
+}
+
+class Manager extends Employee {
+  constructor(name, salary, department, bonus) {
+      super(name, salary); 
+      this.department = department;
+      this.bonus = bonus; 
+  }
+
+  calculateAnnualSalary() {
+      return super.calculateAnnualSalary() + this.bonus;
+  }
+}
+
+const manager1 = new Manager("manthan", 5000, "IT", 10000);
+const manager2 = new Manager("bharat", 6000, "HR", 15000);
+
+
+
+
+console.log(`${manager1.name} (Department: ${manager1.department}) - Annual Salary: $${manager1.calculateAnnualSalary()}`);
+console.log(`${manager2.name} (Department: ${manager2.department}) - Annual Salary: $${manager2.calculateAnnualSalary()}`);
+
+
+// question-7
+
+
+class Book {
+  constructor(title, author, publicationYear) {
+      this.title = title;
+      this.author = author;
+      this.publicationYear = publicationYear;
+  }
+
+  displayDetails() {
+      return `Title: ${this.title}Author: ${this.author}Publication Year: ${this.publicationYear}`;
+  }
+}
+
+class Ebook extends Book {
+  constructor(title, author, publicationYear, price) {
+      super(title, author, publicationYear);
+      this.price = price;
+  }
+
+  displayDetails() {
+      return `${super.displayDetails()}Price: $${this.price.toFixed(2)}`;
+  }
+}
+
+const myEbook = new Ebook("JavaScript Mastery", "manhotra", 2022, 15.99);
+
+console.log(myEbook.displayDetails());
+
+
+
+// question-8
+
+class Animal {
+  constructor(species, sound) {
+      this.species = species;
+      this.sound = sound;
+  }
+
+  makeSound() {
+      console.log(`${this.species} makes a sound: ${this.sound}`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(species, sound, color) {
+      super(species, sound); 
+      this.color = color;
+  }
+
+  makeSound() {
+      console.log(`${this.color} ${this.species} says: ${this.sound}`);
+  }
+}
+
+const myDog = new Dog('Dog', 'Woof', 'Black');
+
+myDog.makeSound();
+
+
+
+
